d887576a25c1be033a7a362b341d9f586590927d
Juil <dev@juil.me>
2023-04-11 22:07:40 -0400

    Generate account and write accont balance

--- krypto_jobs.py
+++ krypto_jobs.py
@@ -157,7 +157,7 @@ st.sidebar.markdown("## Client Account Address and Ethernet Balance in Ether")
 
 # @TODO:
 #  Call the `generate_account` function and save it as the variable `account`
-# YOUR CODE HERE
+account = generate_account()
 
 ##########################################
 
@@ -173,7 +173,7 @@ st.sidebar.write(account.address)
 # @TODO
 # Call `get_balance` function and pass it your account address
 # Write the returned ether balance to the sidebar
-# YOUR CODE HERE
+st.sidebar.write(get_balance(w3, account.address))
 
 ##########################################
 
